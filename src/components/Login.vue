e77beae3561040217408165f7921c878a1ae9e4c
John Griebel <john@johngriebel.com>
2019-06-06 23:59:16 -0400

    Super basic JWT authentication in place
    Signed-off-by: John Griebel <john@johngriebel.com>

---
+++ src/components/Login.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="login">
+        <div class="field">
+            <p class="control">
+                <input class="input" type="text" placeholder="Username" v-model="username">
+            </p>
+        </div>
+        <div class="field">
+            <p class="control">
+                <input class="input" type="password" placeholder="Password" v-model="password">
+            </p>
+        </div>
+        <div class="field">
+            <p class="control">
+                <button class="button" v-on:click="loginClicked">Login</button>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: "Login",
+        data() {
+            return {
+                username: "",
+                password: "",
+            }
+        },
+        methods: {
+            loginClicked: function (event) {
+                console.log(this.username);
+                console.log(this.password);
+                axios.post('http://localhost:8000/api-token-auth/', {
+                    username: this.username,
+                    password: this.password
+                    }
+                ).then(response => {
+                    console.log(response.data.token);
+                    // Assuming success in every case. It's a demo; sue me.
+                    localStorage.setItem('token', response.data.token);
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
